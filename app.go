496ba633b4d3cb2bb69d639ba10c6f8e52d16aa3
Александр <dronline872@gmail.com>
2021-10-01 10:26:55 +0300

    доработка калькулятора

--- app.go
+++ app.go
@@ -1,43 +1,118 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	calcAdd := calc(2, 2, "add")
-	calcAddition := calc(3, 6, "add")
-	calcMult := calc(7, 7, "mult")
-	calcDiv := calc(9, 3, "div")
-	fmt.Printf("2 + 2 = %d, 3 + 6 = %d, 7 * 7 = %d, 9 / 3 = %d", calcAdd, calcAddition, calcMult, calcDiv)
-}
-
-func calc(a int, b int, t string) int {
-	ch := make(chan int)
-	switch t {
-	case "add":
-		go addition(a, b, ch)
-	case "subs":
-		go substraction(a, b, ch)
-	case "mult":
-		go multiplication(a, b, ch)
-	case "div":
-		go division(a, b, ch)
+	var value string
+	fmt.Println("Введите пример в формате a+b, доступны операторы: +, -, *, / ")
+	fmt.Scan(&value)
+	operator, err := checkOperator(value)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	return <-ch
+	arrNumbers, err := operands(value)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	result, err := calc(arrNumbers, operator)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Результат: %s=%.2f", value, result)
 }
 
-func addition(a, b int, ch chan int) {
-	ch <- a + b
+//калькулятор
+func calc(num []float64, t string) (result float64, err error) {
+	if len(num) != 2 {
+		err = fmt.Errorf("Должно быть два операнда")
+		return
+	}
+
+	ch := make(chan float64)
+	switch t {
+	case "+":
+		go addition(num, ch)
+	case "-":
+		go substraction(num, ch)
+	case "*":
+		go multiplication(num, ch)
+	case "/":
+		go division(num, ch)
+	}
+
+	result = <-ch
+
+	return
 }
 
-func substraction(a, b int, ch chan int) {
-	ch <- a - b
+//Возврат операндов
+func operands(str string) (result []float64, err error) {
+	var num float64
+	words := regexp.MustCompile("[+,*,-,/]{1}").Split(str, -1)
+	for _, v := range words {
+		num, err = strconv.ParseFloat(v, 64)
+		if err != nil {
+			err = fmt.Errorf("Не является числом, ошибка: %s\n", err)
+			return
+		}
+		result = append(result, num)
+	}
+
+	return
 }
 
-func multiplication(a, b int, ch chan int) {
-	ch <- a * b
+//Сложение
+func addition(num []float64, ch chan float64) {
+	ch <- num[0] + num[1]
 }
 
-func division(a, b int, ch chan int) {
-	ch <- a / b
+//Вычитание
+func substraction(num []float64, ch chan float64) {
+	ch <- num[0] - num[1]
+}
+
+//Умножение
+func multiplication(num []float64, ch chan float64) {
+	ch <- num[0] * num[1]
+}
+
+//Деление
+func division(num []float64, ch chan float64) {
+	if num[1] == 0 {
+		fmt.Println("Нельзя делить на ноль")
+		os.Exit(1)
+	}
+
+	ch <- num[0] / num[1]
+}
+
+//проверка количества операторов, возврат текущего оператора
+func checkOperator(str string) (operator string, err error) {
+	countOperator := 0
+	operators := [...]string{"+", "-", "*", "/"}
+	for _, v := range operators {
+		countOperator += strings.Count(str, v)
+		if countOperator == 1 && operator == "" {
+			operator = v
+		}
+	}
+
+	if countOperator != 1 {
+		err = fmt.Errorf("Недопустимое количество операторов:%d. Должен быть один оператор", countOperator)
+		return
+	}
+
+	return
 }
